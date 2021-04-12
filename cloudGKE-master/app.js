80a56bee2a201afa2e73d9fec73937d15dae028d
Evelyn W <71800800+evewinny@users.noreply.github.com>
2021-04-12 23:51:17 +0800

    Update app.js

--- cloudGKE-master/app.js
+++ cloudGKE-master/app.js
@@ -7,9 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Nama, evelyn winny thody\n')
-    res.send('Jurusan Sistem Informasi\n')
-    res.send('NIM 51018005\n')
+    res.send('Nama, evelyn winny thody\n Jurusan Sistem Informasi\n NIM 51018005\n')
 })
 
 app.listen(PORT, HOST)
